4f61fc5c7987d5d688e23453225b1620f555bdf0
sagecompscie <sagereid32@gmail.com>
2025-10-04 16:05:54 -0600

    testing

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import {loadHeaderFooter} from "./util.mjs";
 import PokeData from "./PokeData.mjs";
 import PokeList from "./PokeList.mjs";
 
-loadHeaderFooter();
+// loadHeaderFooter();
 
 const dataSource = new PokeData();
 const listElement = document.querySelector(".poke-list")
